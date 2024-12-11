082d37b8c9a96a35aacfa4f3591fad1c790acbd9
raghav-dwivedi <raghav.dwivedi241@gmail.com>
2024-12-11 13:53:43 +0530

    update: use a common component for all the loading screens

--- src/app/(dashboard)/(routes)/loading.tsx
+++ src/app/(dashboard)/(routes)/loading.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-import Image from "next/image";
-import { useState, useEffect } from "react";
+import DefaultLoadingScreen from "@/components/default-loading-screen";
 
 const messages = [
   "Hold on! We’re gathering your strengths and areas for improvement.",
@@ -10,44 +7,5 @@ const messages = [
 ];
 
 export default function Loading() {
-  const [currentMessage, setCurrentMessage] = useState(
-    messages[Math.floor(Math.random() * messages.length)]
-  );
-  const [typedMessage, setTypedMessage] = useState("");
-  const [messageIndex, setMessageIndex] = useState(
-    Math.floor(Math.random() * messages.length)
-  );
-
-  useEffect(() => {
-    const typeMessage = (message: string, idx: number = 0) => {
-      if (idx <= message.length) {
-        setTypedMessage(message.slice(0, idx));
-        setTimeout(() => typeMessage(message, idx + 1), 100);
-      } else {
-        setTimeout(() => {
-          const nextIndex = (messageIndex + 1) % messages.length;
-          setMessageIndex(nextIndex);
-          setTypedMessage("");
-          setCurrentMessage(messages[nextIndex]);
-        }, 2000);
-      }
-    };
-
-    typeMessage(currentMessage);
-  }, [currentMessage, messageIndex]);
-
-  return (
-    <main className="loading-container">
-      <div>
-        <Image
-          src="/images/gifs/loading-screen-animation.gif"
-          alt="loading-logo"
-          height={64}
-          width={64}
-          className="loading-icon"
-        />
-      </div>
-      <h2 className="loading-text">{typedMessage}</h2>
-    </main>
-  );
+  return <DefaultLoadingScreen messages={messages} />;
 }
