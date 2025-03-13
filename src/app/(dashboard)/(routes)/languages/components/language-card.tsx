885a7d008d194ecd182716ef13633fb18d19cbf0
Aaryan6 <aaryanpatel683@gmail.com>
2025-03-13 12:51:40 +0530

    fix: questions update issue for language card

--- src/app/(dashboard)/(routes)/languages/components/language-card.tsx
+++ src/app/(dashboard)/(routes)/languages/components/language-card.tsx
@@ -7,6 +7,10 @@ import { ArrowRight } from "lucide-react";
 import LanguageImage from "@/public/images/icons/language-card.svg";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { getLanguageTopics } from "@/actions/language.actions";
+import { getCookie } from "cookies-next";
+import { useQuery } from "@tanstack/react-query";
+import LanguageCardSkeleton from "./language-card-skeleton";
 
 export default function LanguageCard({
   lang,
@@ -18,20 +22,39 @@ export default function LanguageCard({
   totalQuestions: { id: number; language_id: number }[];
 }) {
   const router = useRouter();
-  const [completedQuestions, setCompletedQuestions] = useState(0);
   const totalQuestionsCount = totalQuestions.filter(
     (item) => item.language_id === langId
   ).length;
+  const userId = getCookie("userId");
+  const [completedQuestions, setCompletedQuestions] = useState(0);
+  const { data, isLoading } = useQuery({
+    queryKey: ["language_topics", langId, userId],
+    queryFn: async () => {
+      if (!userId) return null;
+      return await getLanguageTopics({ langId, userId });
+    },
+    retry: false,
+  });
 
   useEffect(() => {
-    const storedValue = localStorage.getItem(`totalCorrectAnswers_${langId}`);
-    setCompletedQuestions(Number(storedValue) || 0);
-  }, [langId]);
+    let totalCorrect = 0;
+    if (!data) return;
+    for (const topic of data) {
+      if (topic.languages_quiz && topic.languages_quiz.length > 0) {
+        for (const quiz of topic.languages_quiz) {
+          totalCorrect += quiz.correct;
+        }
+      }
+    }
+    setCompletedQuestions(totalCorrect);
+  }, [data]);
 
   const progress =
     totalQuestionsCount === 0
       ? 0
-      : (completedQuestions / totalQuestionsCount) * 100;
+      : ((completedQuestions || 0) / totalQuestionsCount) * 100;
+
+  if (isLoading) return <LanguageCardSkeleton />;
 
   return (
     <div className="bg-[#F5F9FF] shadow-[0px_0px_8px_0px_#0053F429] p-8 rounded-xl max-w-xl w-full mx-auto my-10 flex items-center gap-8">
